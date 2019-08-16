6dbd210c4a82ac6b77ab741712101d3ce6f749bf
kaanyola <kaanyola@gmail.com>
2019-08-16 23:31:02 +0300

    second commit

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+def index(request):
+    context = {
+        "numbers" : [1,2,3,4,5,6,7],
+    }
+    return render(request, "index.html", context)
