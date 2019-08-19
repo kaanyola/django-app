64f8327b66d6c0216d354de7deaa643e371c1110
kaanyola <kaanyola@gmail.com>
2019-08-20 01:12:37 +0300

    commit"

--- djangoapp/urls.py
+++ djangoapp/urls.py
@@ -20,5 +20,7 @@ from todo import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
-    path('addTodo/', views.addTodo)
+    path('addTodo', views.addTodo),
+    path('update/<int:id>', views.update),
+    path('delete/<int:id>', views.delete)
 ]
